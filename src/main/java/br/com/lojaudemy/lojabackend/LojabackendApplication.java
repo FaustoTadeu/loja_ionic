a70a233f5043d5fb26c9149d8460df3b54cf8b2c
FaustoTadeu <fausto.tna@hotmail.com>
2018-12-11 22:29:51 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/lojaudemy/lojabackend/LojabackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.lojaudemy.lojabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojabackendApplication.class, args);
+    }
+}
